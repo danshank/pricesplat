a5f98dd5894e7b62ad5d329b71a7a6a5afdce450
Greg Monteleone <gmonte648@gmail.com>
2025-07-09 19:00:36 -0400

    adding server dir/reorganizing fe files into app dir

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact()],
+  plugins: [tanstackRouter({ autoCodeSplitting: true, routesDirectory:'./src/app/routes', generatedRouteTree:'./src/app/routeTree.gen.ts' }), viteReact()],
   test: {
     globals: true,
     environment: 'jsdom',
